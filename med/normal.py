ba1244900df96e5c3bc6bd1bfcc98fab2f629a8f
codebuff <nonfatalist@gmail.com>
2015-10-11 15:00:33 +0530

    normal med

--- med/normal.py
+++ med/normal.py
@@ -16,20 +16,24 @@ s 7
 t 8
 r 9                    MED
 
-MED = dist(len(given_str) - 1 ,len(desired_str) - 1 )
+MED = distance[len(given_str) - 1][len(desired_str) - 1]
 """
 
 # initialize first row  ie dist(0, j)
-dist = [[desired_str[j] for j in range(len(desired_str))]]
-
-print(dist)
+distance = [[j for j in range(len(desired_str) + 1)]]
 
 # initialize first column ie dist(j, 0)
-# using conditional expression ie ternary operators
-# for i in range(len(given_str)):
-#    dist.append([given_str[i] if k == 0 else -1 for k in range(len(desired_str))])
-
 # using (nested) list comprehension and ternary operator
-dist += [[given_str[i] if k == 0 else -1 for k in range(len(desired_str))] for i in range(len(given_str))]
-
-print(dist)
+distance += [[i if k == 0 else -1 for k in range(len(desired_str) + 1)] for i in range(1, len(given_str) + 1)]
+for i in range(1, len(given_str) + 1):
+    for j in range(1, len(desired_str) + 1):
+        paths = list([distance[i - 1][j] + 1])
+        paths.append(distance[i][j - 1] + 1)
+        if given_str[i - 1] == desired_str[j - 1]:
+            paths.append(distance[i - 1][j - 1])
+        else:
+            paths.append(distance[i - 1][j - 1] + 2)
+        distance[i][j] = min(paths)
+        print(paths)
+print(distance)
+print(distance[len(given_str) - 1][len(desired_str) - 1])
